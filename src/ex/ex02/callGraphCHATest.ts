6c1a55fe7b23fcb0b1dc5c847a7336f4d26bb763
code-sunbo <bb.sunbo@huawei.com>
2024-10-25 10:29:43 +0800

    update call graph set entry code.
    Signed-off-by: code-sunbo <bb.sunbo@huawei.com>

--- src/ex/ex02/callGraphCHATest.ts
+++ src/ex/ex02/callGraphCHATest.ts
@@ -6,26 +6,18 @@ config.buildFromProjectDir('src/ex/resources/callGraph');
 function runScene(config: SceneConfig) {
     let projectScene: Scene = new Scene();
     projectScene.buildSceneFromProjectDir(config);
+    
     let entryPoints: MethodSignature[] = [];
-
     // 指定入口点函数
-    for (let arkFile of projectScene.getFiles()) {
-        if (arkFile.getName() === 'main.ts') {
-            for (let arkClass of arkFile.getClasses()) {
-                if (arkClass.getName() === '_DEFAULT_ARK_CLASS') {
-                    for (let arkMethod of arkClass.getMethods()) {
-                        if (arkMethod.getName() === 'main') {
-                            entryPoints.push(arkMethod.getSignature());
-                        }
-                    }
-                }
-            }
+    for (const method of projectScene.getMethods()) {
+        if (method.getName() === 'main' && method.getDeclaringArkFile().getName().endsWith('main.ts')) {
+            entryPoints.push(method.getSignature());
         }
     }
 
     // 类型推导
     projectScene.inferTypes();
-
+    
     // 构建方法调用图
     let callGraph = projectScene.makeCallGraphCHA(entryPoints);
     callGraph.dump('./out/CHA.dot');
--- src/ex/ex02/callGraphRTATest.ts
+++ src/ex/ex02/callGraphRTATest.ts
@@ -6,25 +6,16 @@ config.buildFromProjectDir('src/ex/resources/callGraph');
 function runScene(config: SceneConfig) {
     let projectScene: Scene = new Scene();
     projectScene.buildSceneFromProjectDir(config);
-    let entryPoints: MethodSignature[] = [];
-
-    // 指定入口点函数
-    for (let arkFile of projectScene.getFiles()) {
-        if (arkFile.getName() === 'main.ts') {
-            for (let arkClass of arkFile.getClasses()) {
-                if (arkClass.getName() === '_DEFAULT_ARK_CLASS') {
-                    for (let arkMethod of arkClass.getMethods()) {
-                        if (arkMethod.getName() === 'main') {
-                            entryPoints.push(arkMethod.getSignature());
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     // 类型推导
     projectScene.inferTypes();
+
+    let entryPoints: MethodSignature[] = [];
+    // 指定入口点函数
+    for (const method of projectScene.getMethods()) {
+        if (method.getName() === 'main' && method.getDeclaringArkFile().getName().endsWith('main.ts')) {
+            entryPoints.push(method.getSignature());
+        }
+    }
 
     // 构建方法调用图
     let callGraph = projectScene.makeCallGraphRTA(entryPoints);
