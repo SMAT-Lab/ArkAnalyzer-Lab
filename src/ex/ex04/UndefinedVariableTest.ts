09c55acaf3ee216090f33c338ec9a334557e42bd
code-sunbo <bb.sunbo@huawei.com>
2024-10-28 11:18:30 +0800

    update undefined variable test.
    Signed-off-by: code-sunbo <bb.sunbo@huawei.com>

--- src/ex/ex04/UndefinedVariableTest.ts
+++ src/ex/ex04/UndefinedVariableTest.ts
@@ -1,4 +1,4 @@
-import { Scene, SceneConfig, ModelUtils, UndefinedVariableChecker, UndefinedVariableSolver } from 'arkanalyzer';
+import { Scene, SceneConfig, UndefinedVariableChecker, UndefinedVariableSolver } from 'arkanalyzer';
 
 const projectRoot = 'src/ex/resources/UndefinedVariable';
 let config: SceneConfig = new SceneConfig();
@@ -6,12 +6,16 @@ config.buildFromProjectDir(projectRoot);
 let scene = new Scene();
 scene.buildSceneFromProjectDir(config);
 
-const method = scene.getMethods().filter((v) => v.getName() === 'U2')[0];
-if (method) {
-    const problem = new UndefinedVariableChecker(
-        [...method.getCfg()!.getBlocks()][0].getStmts()[method.getParameters().length],
-        method
-    );
-    const solver = new UndefinedVariableSolver(problem, scene);
-    solver.solve();
+const method = scene.getMethods().filter(
+    (v) => v.getName() === 'U2')[0];
+const problem = new UndefinedVariableChecker(
+    [...method.getCfg()!.getBlocks()][0].getStmts()[method.getParameters().length],
+    method
+);
+const solver = new UndefinedVariableSolver(problem, scene);
+solver.solve();
+
+for (const outcome of problem.getOutcomes()) {
+    let position = outcome.stmt.getOriginPositionInfo();
+    console.log('undefined error in line ' + position.getLineNo());
 }
--- src/ex/resources/UndefinedVariable/test2.ts
+++ src/ex/resources/UndefinedVariable/test2.ts
@@ -1,8 +1,13 @@
 import { property } from './test';
+
 class static2 {
     static p: property;
 }
 
 export function im() {
     console.log(static2.p.pp);
+}
+
+namespace f2n {
+    let lo = 0;
 }
